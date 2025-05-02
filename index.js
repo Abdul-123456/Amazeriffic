937e5b7fa0c814bbda7a898645a8eaa989367f31
Abdul-123456 <razzakabdul816@gmail.com>
2025-05-02 02:42:54 -0400

    Update index.js

--- index.js
+++ index.js
@@ -41,7 +41,7 @@ jwtVerifyPromise = (cookie, token) => {
     });
 };
 
-app.listen(PORT, () => console.log('Server running on port ' + PORT));
+app.listen(PORT, () => console.log('Server running on: http://localhost:4000/'));
 app.use(express.static(__dirname));
 app.use(express.urlencoded({extended: 'false'}));
 app.use(express.json());
